ed12e51ac3fa39a25f50ecc82c632cdeb54b13a3
liviaeffrateles <163459085+liviaeffrateles@users.noreply.github.com>
2024-03-25 10:59:02 -0300

    Script_simulation.R

--- R/Script_micurus_tese_novo_26_02_2024_10_22.R
+++ R/Script_micurus_tese_novo_26_02_2024_10_22.R
@@ -1,12 +1,12 @@
-#Ler pacotes
+#library packages
 library(picante)
 library(ape)
 library(phytools)
-library(beepr)#Pacote som
+library(beepr)
 library(openxlsx)
 library(caper)
 
-#Carregar filogenia 
+#phylogeny 
 micrurus_tree<-read.nexus("sunplin_Micrurus.txt")
 vazio<-numeric()
 
@@ -22,18 +22,18 @@ vazio<-numeric()
                           "R2_lat_depois","lambda_lat_depois","alpha_lat_depois","beta_lat_depois",
                           "R2_quad_lat_depois","lambda_quad_lat_depois","alpha_quad_lat_depois","beta1_quad_lat_depois","beta2_quad_lat_depois",
                           "split","index_run","index_tree")
-      s<-read.xlsx("Vetor.xlsx",1)#pesos (Incerteza taxonômica) de cada espécie de micurus
-      s$Vetor<-s$ITU*1.5#Calibrando para ter 40 quebras
+      s<-read.xlsx("Vetor.xlsx",1) #weights (Index of Taxonomic Uncertainty) of each Micrurus species
+      s$Vetor<-s$ITU*1.5 #calibrating to have 40 splits
       s<-s[,c(1,4,6,7,9)]
-      # Calcular Diversificação -------------------------------------------------
+      # calculate Diversification -------------------------------------------------
       
-      ##Calcular tip rates (viés presente)
+      #calculate tip rates
       equal_splits<- evol.distinct(tree, type = "equal.splits", 
-                                   scale = FALSE, use.branch.lengths = TRUE)#Calcular indíce de diversidade genética
-      tip_dr <- 1/equal_splits$w # inverse of equal splits (taxa de especiação de cada ponta)
+                                   scale = FALSE, use.branch.lengths = TRUE) #calculate genetic diversity
+      tip_dr <- 1/equal_splits$w # inverse of equal splits (speciation rate of each tip)
       
       
-      # Exportando variáveis antes das quebras ----------------------------------
+      # exporting variables before the splits ----------------------------------
       # output
       #R^2 Dr~lat
       lat= as.numeric(decostand(s$LatC,method="standardize"))
@@ -41,7 +41,7 @@ vazio<-numeric()
                      lat=as.numeric(decostand(s$LatC,method="standardize")),
                      lat2=as.numeric(decostand(s$LatC^2,method="standardize")),
                      tip_dr)
-      tryCatch({#Caso ocorre erro de otimização
+      tryCatch({ #in case an optimization error occurs
         res_mod_lat <- summary(pgls(tip_dr~lat,comparative.data(tree,X,"Species"), lambda="ML"))
         res_mod_quad_lat <- summary(pgls(tip_dr~lat+lat2,comparative.data(tree,X,"Species"), lambda="ML"))
         
@@ -57,29 +57,29 @@ vazio<-numeric()
         output[1:100,8] <- res_mod_quad_lat$coefficients[2,1]
         output[1:100,9] <- res_mod_quad_lat$coefficients[3,1]
       }, error = function(err) {
-        cat("Erro capturado:", conditionMessage(err), "\n") # O loop continuará mesmo que ocorra um erro
+        cat("Erro capturado:", conditionMessage(err), "\n") #the loop will continue even if an error occurs
       })
        for (j in 1:100) {
          tree <- force.ultrametric(micrurus_tree[[i]])
-    #Carregar vetores
-    s<-read.xlsx("Vetor.xlsx",1)#pesos (Incerteza taxonômica) de cada espécie de micurus
+    #vectors
+    s<-read.xlsx("Vetor.xlsx",1) #weights (Index of Taxonomic Uncertainty) of each Micrurus species
     s$Vetor<-s$ITU*1.5
     s<-s[,c(1,4,6,7,9)]
-#Reajustar árvore
-test <-runif(Ntip(tree),0,1)#Criar vetor de testagem para splits
-##Adotar escolha de de split para cada ponta 
-quebra<-ifelse(test < s$Vetor, 1,0)#Vetor, no qual 1= terá split e 0=não terá split
-names(quebra)<-tree$tip.label#Nomes dos resultados de quebra serão as espécies
+#adjust tree
+test <-runif(Ntip(tree),0,1) #create a testing vector for splits
+#adopt split choice for each tip 
+quebra<-ifelse(test < s$Vetor, 1,0) #vector, where 1 = will have split and 0 = will not have split
+names(quebra)<-tree$tip.label #the names of the splits results will be the species
 
-##Descrição de novas espécies e/ou adição de dados moleculares de espécies que não tinham
-tip<-paste0("?",1:sum(quebra))#Salvar espécies potenciais ainda não descobertas
-sister<-names(quebra[quebra==1])#Salvar quais espécies serão irmãs de cada uma ainda não descoberta
-where<-numeric()#criar vetor local vazio
+#description of new species
+tip<-paste0("?",1:sum(quebra)) #save potential undescribed species
+sister<-names(quebra[quebra==1]) #save which species will be sister taxa of each undescribed species
+where<-numeric() #create empty local vector
 
  novas<-data.frame(Species=tip,LatMin=NA,LatMax=NA,LatC=NA,Vetor=runif(length(tip),0,1))
  A<-numeric()
- for(n in 1:length(sister)){#loop de 1 até o número de splits atual
-  where<-c(where,which(tree$tip.label==sister[n]))# salvar posição de cada nova descoberta
+ for(n in 1:length(sister)){ #loop from 1 to the current number of splits
+  where<-c(where,which(tree$tip.label==sister[n])) #save the position of each new description
   
   A<-c(A,sample(seq(0.1,1,0.1),1))
   
@@ -95,15 +95,15 @@ where<-numeric()#criar vetor local vazio
     novas$LatC[n]<-(novas$LatMax[n] + novas$LatMin[n])/2
   }
   
-  #Qubre as espécies
+  #split the species
   tree_LTG <-bind.tip(tree,tip[n],where=where[n],
-                      position=runif(1,0.25,0.75)*tree$edge.length[which(tree$edge[,2]==where[n])])# Adicionar nova ponta ao lado da ponta irmã
-  tree<-tree_LTG#Atualizar árvore
-}#fechar for dos splits 
+                      position=runif(1,0.25,0.75)*tree$edge.length[which(tree$edge[,2]==where[n])]) #add new tip close to the sister tip
+  tree<-tree_LTG #update the tree
+} #close the loop for the splits
  
- #Novo Tip_rates
-equal_splits.final <- evol.distinct(tree, type = "equal.splits", scale = FALSE, use.branch.lengths = TRUE)#calcular estimativa de importância filogenética
-tip_dr.final <- 1/equal_splits.final$w # inverse of equal splits
+ #new tip rates
+equal_splits.final <- evol.distinct(tree, type = "equal.splits", scale = FALSE, use.branch.lengths = TRUE)
+tip_dr.final <- 1/equal_splits.final$w #inverse of equal splits
 s_final<-rbind(s,novas)
 dados <- s_final[order(match(s_final$Species, tree$tip.label)), ]
 
@@ -113,7 +113,7 @@ X_final <-data.frame(Species=dados$Species,
                      lat=as.numeric(decostand(dados$LatC,method="standardize")),
                      lat2=as.numeric(decostand(dados$LatC^2,method="standardize")),
                      tip_dr.final)
-tryCatch({#Caso ocorre erro de otimização
+tryCatch({ #in case an optimization error occurs
   res_mod_lat2 <- summary(pgls(tip_dr.final~lat,comparative.data(tree,X_final,"Species"), lambda="ML"))
   res_mod_quad_lat2 <- summary(pgls(tip_dr.final~lat+lat2,comparative.data(tree,X_final,"Species"), lambda="ML"))
   
@@ -129,7 +129,7 @@ tryCatch({#Caso ocorre erro de otimização
   output[j,18] <- res_mod_quad_lat2$coefficients[3,1]
 
   }, error = function(err) {
-    cat("Erro capturado:", conditionMessage(err), "\n") # O loop continuará mesmo que ocorra um erro
+    cat("Erro capturado:", conditionMessage(err), "\n") #the loop will continue even if an error occurs
   })
 output[j,19]<-n
 output[j,20]<-j
@@ -138,7 +138,7 @@ print(paste(j,"off",100))
   output[1:20,21]<-i
   vazio<-rbind(vazio,output)
   print(paste(i,"off",1000))
-  write.table(vazio,"resul_livia_paper2.csv",row.names = F) 
+  write.table(vazio,"Simulation_results.csv",row.names = F) 
   
 }
 
